ca211daa4bfc7ad52df36e811fe229bea2948af6
topstart <topstart00416@gmail.com>
2025-03-26 07:25:13 -0400

    first

--- src/app/(dashboard)/about/page.tsx
+++ src/app/(dashboard)/about/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <h1>About page!</h1>
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+import { SparklesCore } from "@/components/sparkles";
+
+export default function Home() {
+  return (
+    <main
+      className="min-h-screen bg-black/[0.96] antialiased bg-cover bg-center relative overflow-hidden"
+      // style={{
+      //   backgroundImage: "url('https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=2069&auto=format&fit=crop')",
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      // }}
+    >
+      {/* Ambient background with moving particles */}
+      <div className="h-full w-full absolute inset-0 z-0">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
+  );
 }
